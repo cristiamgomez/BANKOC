064f93eaeeae5fcc993bc89c3bbfcf332ca60884
ytapiasa <yeisonalexis1998@gmail.com>
2023-10-28 11:37:38 -0500

    Update Consul_movi.js

--- Consul_movi.js
+++ Consul_movi.js
@@ -1,52 +1,62 @@
-/*
+let sumaTotal = 0;
+const movimientos = []; // Arreglo para almacenar los movimientos
 
-document.addEventListener('DOMContentLoaded', function () {
-    const consul_movi = document.getElementById('consul_movi');
+const consignar = document.getElementById('consignar');
+consignar.addEventListener('click', function() {
+    const valorIngresado = prompt("Cuanto deseas consignar");
 
-    consul_movi.addEventListener('submit', function (event) {
-        event.preventDefault();
+    if (valorIngresado !== null && !isNaN(valorIngresado)) {
+        const numero = parseFloat(valorIngresado);
+        sumaTotal += numero;
 
-        // Obtener los valores de los campos
-        const Withdraw = document.getElementById('Withdraw').value;
-        const Search = document.getElementById('Search').value;
-        const Send = document.getElementById('Send').value;
-        const Change = document.getElementById('Change').value;
-        const Check_move = document.getElementById('Check_move').value;
-        const Exit = document.getElementById('Exit').value;
+        movimientos.push(`Consignación de ${numero} pesos`);
+        console.log(`Consignación de ${numero} pesos`);
 
-        // Realizar validación básica
-        if (Withdraw === '' || Search === '' || Send === '' || Change === '' || Check_move === '' || Exit === '') {
-            console.log('Por favor complete todos los campos.');
-            return; // Detener el proceso si algún campo está vacío
-        }
+        const valorTotalMostrado = document.getElementById('valorTotal');
 
-        // Realizar alguna acción con los valores capturados
-        console.log('Valores capturados:');
-        console.log('Withdraw:', Withdraw);
-        console.log('Search:', Search);
-        console.log('Send:', Send);
-        console.log('Change:', Change);
-        console.log('Check_move:', Check_move);
-        console.log('Exit:', Exit);
-
-        // Aquí podrías enviar los datos a través de una solicitud AJAX, procesarlos, etc.
-        // Por ejemplo, puedes realizar una petición fetch para enviar los datos a un servidor.
-        // fetch('url_del_servidor', {
-        //     method: 'POST',
-        //     body: JSON.stringify({ Withdraw, Search, Send, Change, Check_move, Exit }),
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-        // })
-        // .then(response => response.json())
-        // .then(data => {
-        //     console.log('Respuesta del servidor:', data);
-        // })
-        // .catch(error => {
-        //     console.error('Error al enviar los datos:', error);
-        // });
-    });
+        alert(`El valor ${numero} ha sido consignado con éxito.`);
+    } else {
+        console.log("No se ingresó un número válido.");
+    }
 });
 
-/*
+const retirar = document.getElementById('retirar');
+retirar.addEventListener('click', function() {
+    const valorRetirar = prompt("Valor a retirar");
 
+    if (valorRetirar !== null && !isNaN(valorRetirar)) {
+        const numeroRetiro = parseFloat(valorRetirar);
+        if (numeroRetiro <= sumaTotal) {
+            sumaTotal -= numeroRetiro;
+
+            // Registrar el movimiento de retiro
+            movimientos.push(`Retiro de ${numeroRetiro} pesos`);
+            console.log(`Retiro de ${numeroRetiro} pesos`);
+
+            const valorTotalMostrado = document.getElementById('valorTotal');
+
+            alert(`Se ha retirado ${numeroRetiro} con éxito.`);
+        } else {
+            alert("No tienes suficiente dinero para realizar este retiro.");
+        }
+    } else {
+        console.log("No se ingresó un número válido para el retiro.");
+    }
+});
+
+const botonConsultar = document.getElementById('consultar');
+botonConsultar.addEventListener('click', function() {
+    alert(`En tu cuenta hay ${sumaTotal} Pesos`);
+});
+
+const mostrarMovimientos = document.getElementById('movimientos');
+mostrarMovimientos.addEventListener('click', function() {
+    movimientos.forEach(movimiento => {
+    });
+
+    let listaMovimientos = "Movimientos:\n";
+    movimientos.forEach(movimiento => {
+        listaMovimientos += `- ${movimiento}\n`;
+    });
+    alert(listaMovimientos);
+});

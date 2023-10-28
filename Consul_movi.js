37ba4cd64d3ac5509a85c9403a5e77b790b5e4d1
cristiamgomez <cristiam05gomez@gmail.com>
2023-10-28 11:14:54 -0500

    Update Consul_movi.js

---
+++ Consul_movi.js
@@ -0,0 +1,18 @@
+Document.addEventListener('DOMContentLoaded', function (){
+    const consul_movi = document.getElementById('consul_movi');
+
+    consul_movi.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Obtener los valores de los campos
+        const Withdraw = document.getElementById('Withdraw').value;
+        const Search = document.getElementById('Search').value;
+        const Send = document.getElementById('Send').value;
+        const Change = document.getElementById('Change').value;
+        const Check_move = document.getElementById('Check_move').value;
+        const Exit = document.getElementById('Exit'). value;
+
+        // Realizar validación básica
+        
+    });
+});

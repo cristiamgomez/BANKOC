d139770ed5401b03687354b9f83e025df4622319
cristiamgomez <cristiam05gomez@gmail.com>
2023-11-03 11:12:56 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,33 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const users = []; // Vector para almacenar usuarios
 
-    const registrationForm = document.getElementById('registrationForm');
-    const loginButton = document.getElementById('loginButton');
-
-    registrationForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        const identification = document.getElementById('identification').value;
-        const username = document.getElementById('username').value;
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        const confirmPassword = document.getElementById('confirmPassword').value;
-
-        if (identification === '' || username === '' || email === '' || password === '' || confirmPassword === '') {
-            alert('Por favor, complete todos los campos.');
-        } else if (password !== confirmPassword) {
-            alert('Las contraseñas no coinciden.');
-        } else {
-            const newUser = {
-                identification,
-                username,
-                email,
-                password
-            };
-
-            users.push(newUser);
-            alert('Usuario registrado correctamente.');
-        }
-    });
+    
 
     loginButton.addEventListener('click', function () {
         const username = document.getElementById('username').value;

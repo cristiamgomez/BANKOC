8d64020c15751dc9b7de4e4b43a69e63533328a3
cristiamgomez <cristiam05gomez@gmail.com>
2023-11-06 08:24:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function mostrarAnuncio(texto, enlace) {
 
 // Llama a la función para mostrar un anuncio específico
 mostrarAnuncio("¡PROGRAMAS TECNICOS!", "https://www.cesde.edu.co/educaciondigital/");
+
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar el envío predeterminado del formulario
+
+    const buttonClicked = event.submitter; // Obtener el botón de envío que fue presionado
+
+    if (buttonClicked.id === "loginBtn") {
+        window.location.href = "login.html"; // Redirigir a la página de inicio de sesión (log in)
+    } else if (buttonClicked.id === "checkInBtn") {
+        window.location.href = "registro.html"; // Redirigir a la página de check in
+    }
+});
